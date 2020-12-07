6e772957ac9ec7ee83beec56977e29d603512161
Florencia <florencia.r.aramayo@gmail.com>
2020-12-07 17:33:05 -0300

    Commit inicial

---
+++ app.js
@@ -0,0 +1,12 @@
+//tabla de multiplicar
+
+//si despues de const o let vienen llaves quiere decir
+//que es una destructuracion
+const { crearArchivo } = require('./multiplicar/multiplicar');
+let base = 6;
+
+
+multiplicar.crearArchivo
+
+crearArchivo(base)
+    .then(archivo => console.log(`Archivo creado: ${ archivo }`));
